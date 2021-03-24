9b058b6e9495b361e02fa4f689cf744da5991c48
Sergio Herrera <herrera.se1992@gmail.com>
2021-03-24 14:51:33 -0300

    planteamiento segunda parte

--- Prueba.rb
+++ Prueba.rb
@@ -18,7 +18,7 @@ def request(address, api_key="TB1j1Qlk3cXb7jyBO085cD4LizEVOgeCKbSnbcTf")
     JSON.parse response.read_body  #llamamos al Json para ordenar nuestra base de datos
 end
 
-def build_web_page(request)
+def build_web_page(request) # metodo que construlle Nuestra estructura HTML
 
     fotos = request.map{ |filter| filter['img_src'] } # filtramos solo la linea "img_src": "http://mars.....JPG"
     html = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>" # Abrimos formato html, dejamos el head vacio y abrimos el body generando una lista 
@@ -30,6 +30,17 @@ def build_web_page(request)
 
 end
 
-photos = build_web_page(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000')["photos"]) # manipulando el link podemos acotar la cantidad de fotografias cambiando "photos?sol=1000" cambiando el 1000 por el numero de imagenes deseadas
+photos = build_web_page(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100')["photos"]) # manipulando el link podemos acotar la cantidad de fotografias cambiando "photos?sol=1000" cambiando el 1000 por el numero de imagenes deseadas
 
 File.write('mars_Rovers_photos.html', photos.to_s)# le damos a la imagen el formato string para que nos las guarde como url
+
+
+=begin
+def photos_count(request)
+    camera= request.map {|filter| filter[camera][name] }
+    elementos= camera.group_by{ |name|name}
+    print elementos
+end
+
+puts photos_count(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100'))
+=end
