07e2d5a7e3888edfd3ef388cb748dbc68d1d61a4
Sergio Herrera <herrera.se1992@gmail.com>
2021-03-24 12:31:31 -0300

    add appikey

--- Prueba.rb
+++ Prueba.rb
@@ -2,7 +2,7 @@ require "uri"
 require "net/http"
 require "json"
 
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TB1j1Qlk3cXb7jyBO085cD4LizEVOgeCKbSnbcTf")
 
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
@@ -10,6 +10,8 @@ https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 
 response = https.request(request)
-body = JSON.parse response.read_body
-
 puts response.read_body
+
+body = JSON.parse response.read_body  #llamamos al Json para ordenar nuestra base de datos
+
+
